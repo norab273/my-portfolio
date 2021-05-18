a5dd26b52d0fe71272b532406ac2ff2f226ea9f8
Juliette Baron <juliette.baron.33@gmail.com>
2021-05-18 22:59:33 +0200

    cleaning

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   //-- SITE SETTINGS -----
-  author: "@konstantinmuenster",
+  author: "juliettebaron",
   siteTitle: "My Portfolio",
   siteShortTitle: "👩🏻‍💻", // Used as logo text in header, footer, and splash screen
   siteDescription:
@@ -41,38 +41,17 @@ module.exports = {
     primary: "Roboto, Arial, sans-serif",
   },
 
-  //-- ARTICLES SECTION SETTINGS -----
-  // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
-  // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
-  // rssFeed:
-  //   "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konstantinmuenster",
-
-  // shownArticles: 3,
-
   //-- SOCIAL MEDIA SETTINGS -----
-  // There are icons available for the following platforms:
-  // Medium, GitHub, LinkedIn, XING, Behance, E-Mail
+  // There are icons available for GitHub & LinkedIn
   socialMedia: [
     {
       name: "Linkedin",
       url: "https://www.linkedin.com/in/juliette-baron-95984581/",
     },
-    // {
-    //   name: "Medium",
-    //   url: "https://konstantinmuenster.medium.com/",
-    // },
     {
       name: "Github",
       url: "https://github.com/norab273",
     },
-    // {
-    //   name: "Behance",
-    //   url: "https://www.behance.net/konstanmnster",
-    // },
-    // {
-    //   name: "Mail",
-    //   url: "mailto:mail@konstantin.digital",
-    // },
   ],
 
   //-- NAVIGATION SETTINGS -----
